c43c591615b8f71a7656b35b0b73fc2dbc177f80
sk-shiki <88426226+sk-shiki@users.noreply.github.com>
2021-08-04 17:27:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,20 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 bot.run(token)
